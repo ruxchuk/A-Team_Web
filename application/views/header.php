a4db88b0e69a3272cc613df9d20fe40149ddba60
Chukkapun <ruxchuk@gmail.com>
2013-05-03 16:46:13 +0700

    จัด slide

--- application/views/header.php
+++ application/views/header.php
@@ -408,66 +408,60 @@ $baseUrl = base_url();
 <div id="mainmodules1" class="spacer w99">
     <div class="block">
         <div class="module">
-            <div>
-                <div>
-                    <div>
-                        <div id="slidewrap">
-                            <div id="header">
-                                <div class="wrap">
-                                    <div id="slide-holder">
-                                        <div id="slide-runner">
-                                            <a href=""><img id="slide-img-1"
-                                                            src="<?php echo $baseUrl; ?>web/images/nature-photo.png"
-                                                            class="slide" alt=""/></a>
-                                            <a href=""><img id="slide-img-2"
-                                                            src="<?php echo $baseUrl; ?>web/images/nature-photo1.png"
-                                                            class="slide" alt=""/></a>
-                                            <a href=""><img id="slide-img-3"
-                                                            src="<?php echo $baseUrl; ?>web/images/nature-photo2.png"
-                                                            class="slide" alt=""/></a>
-                                            <a href=""><img id="slide-img-4"
-                                                            src="<?php echo $baseUrl; ?>web/images/nature-photo3.png"
-                                                            class="slide" alt=""/></a>
-                                            <a href=""><img id="slide-img-5"
-                                                            src="<?php echo $baseUrl; ?>web/images/nature-photo4.png"
-                                                            class="slide" alt=""/></a>
-                                            <a href=""><img id="slide-img-6"
-                                                            src="<?php echo $baseUrl; ?>web/images/nature-photo4.png"
-                                                            class="slide" alt=""/></a>
-                                            <a href=""><img id="slide-img-7"
-                                                            src="<?php echo $baseUrl; ?>web/images/nature-photo6.png"
-                                                            class="slide" alt=""/></a>
+                <div id="slidewrap">
+                    <div id="slide-header">
+                        <div class="slide-wrap">
+                            <div id="slide-holder">
+                                <div id="slide-runner">
+                                    <a href=""><img id="slide-img-1"
+                                                    src="<?php echo $baseUrl; ?>web/images/nature-photo.png"
+                                                    class="slide" alt=""/></a>
+                                    <a href=""><img id="slide-img-2"
+                                                    src="<?php echo $baseUrl; ?>web/images/nature-photo1.png"
+                                                    class="slide" alt=""/></a>
+                                    <a href=""><img id="slide-img-3"
+                                                    src="<?php echo $baseUrl; ?>web/images/nature-photo2.png"
+                                                    class="slide" alt=""/></a>
+                                    <a href=""><img id="slide-img-4"
+                                                    src="<?php echo $baseUrl; ?>web/images/nature-photo3.png"
+                                                    class="slide" alt=""/></a>
+                                    <a href=""><img id="slide-img-5"
+                                                    src="<?php echo $baseUrl; ?>web/images/nature-photo4.png"
+                                                    class="slide" alt=""/></a>
+                                    <a href=""><img id="slide-img-6"
+                                                    src="<?php echo $baseUrl; ?>web/images/nature-photo4.png"
+                                                    class="slide" alt=""/></a>
+                                    <a href=""><img id="slide-img-7"
+                                                    src="<?php echo $baseUrl; ?>web/images/nature-photo6.png"
+                                                    class="slide" alt=""/></a>
 
-                                            <div id="slide-controls">
-                                                <p id="slide-client" class="text"><strong>post: </strong><span></span>
-                                                </p>
+                                    <div id="slide-controls">
+                                        <p id="slide-client" class="text"><strong>post: </strong><span></span>
+                                        </p>
 
-                                                <p id="slide-desc" class="text"></p>
+                                        <p id="slide-desc" class="text"></p>
 
-                                                <p id="slide-nav"></p>
-                                            </div>
-                                        </div>
-
-                                        <!--content featured gallery here -->
+                                        <p id="slide-nav"></p>
                                     </div>
-                                    <script type="text/javascript">
-                                        if (!window.slider) var slider = {};
-                                        slider.data = [
-                                            {"id": "slide-img-1", "client": "nature beauty", "desc": "nature beauty photography"},
-                                            {"id": "slide-img-2", "client": "nature beauty", "desc": "add your description here"},
-                                            {"id": "slide-img-3", "client": "nature beauty", "desc": "add your description here"},
-                                            {"id": "slide-img-4", "client": "nature beauty", "desc": "add your description here"},
-                                            {"id": "slide-img-5", "client": "nature beauty", "desc": "add your description here"},
-                                            {"id": "slide-img-6", "client": "nature beauty", "desc": "add your description here"},
-                                            {"id": "slide-img-7", "client": "nature beauty", "desc": "add your description here"}
-                                        ];
-                                    </script>
                                 </div>
+
+                                <!--content featured gallery here -->
                             </div>
+                            <script type="text/javascript">
+                                if (!window.slider) var slider = {};
+                                slider.data = [
+                                    {"id": "slide-img-1", "client": "nature beauty", "desc": "nature beauty photography"},
+                                    {"id": "slide-img-2", "client": "nature beauty", "desc": "add your description here"},
+                                    {"id": "slide-img-3", "client": "nature beauty", "desc": "add your description here"},
+                                    {"id": "slide-img-4", "client": "nature beauty", "desc": "add your description here"},
+                                    {"id": "slide-img-5", "client": "nature beauty", "desc": "add your description here"},
+                                    {"id": "slide-img-6", "client": "nature beauty", "desc": "add your description here"},
+                                    {"id": "slide-img-7", "client": "nature beauty", "desc": "add your description here"}
+                                ];
+                            </script>
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
 
     </div>
