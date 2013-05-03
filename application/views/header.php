ec48dd30c801408e6db77cf71c4e9d095db903be
Chukkapun <ruxchuk@gmail.com>
2013-05-03 15:31:22 +0700

    show slide

--- application/views/header.php
+++ application/views/header.php
@@ -398,6 +398,82 @@ $baseUrl = base_url();
 </div>
 <div id="main-content">
 <div id="main-content2">
+
+<link rel="stylesheet" href="<?php echo $baseUrl; ?>web/css/slide-style.css" type="text/css" media="screen"/>
+<script type="text/javascript">var _siteRoot = 'index.html', _root = 'index.html';</script>
+<script type="text/javascript" src="<?php echo $baseUrl; ?>web/js/jquery.js"></script>
+<script type="text/javascript" src="<?php echo $baseUrl; ?>web/js/slide-scripts.js"></script>
+
+<!--slide image-->
+<div id="mainmodules1" class="spacer w99">
+    <div class="block">
+        <div class="module">
+            <div>
+                <div>
+                    <div>
+                        <div id="slidewrap">
+                            <div id="header">
+                                <div class="wrap">
+                                    <div id="slide-holder">
+                                        <div id="slide-runner">
+                                            <a href=""><img id="slide-img-1"
+                                                            src="<?php echo $baseUrl; ?>web/images/nature-photo.png"
+                                                            class="slide" alt=""/></a>
+                                            <a href=""><img id="slide-img-2"
+                                                            src="<?php echo $baseUrl; ?>web/images/nature-photo1.png"
+                                                            class="slide" alt=""/></a>
+                                            <a href=""><img id="slide-img-3"
+                                                            src="<?php echo $baseUrl; ?>web/images/nature-photo2.png"
+                                                            class="slide" alt=""/></a>
+                                            <a href=""><img id="slide-img-4"
+                                                            src="<?php echo $baseUrl; ?>web/images/nature-photo3.png"
+                                                            class="slide" alt=""/></a>
+                                            <a href=""><img id="slide-img-5"
+                                                            src="<?php echo $baseUrl; ?>web/images/nature-photo4.png"
+                                                            class="slide" alt=""/></a>
+                                            <a href=""><img id="slide-img-6"
+                                                            src="<?php echo $baseUrl; ?>web/images/nature-photo4.png"
+                                                            class="slide" alt=""/></a>
+                                            <a href=""><img id="slide-img-7"
+                                                            src="<?php echo $baseUrl; ?>web/images/nature-photo6.png"
+                                                            class="slide" alt=""/></a>
+
+                                            <div id="slide-controls">
+                                                <p id="slide-client" class="text"><strong>post: </strong><span></span>
+                                                </p>
+
+                                                <p id="slide-desc" class="text"></p>
+
+                                                <p id="slide-nav"></p>
+                                            </div>
+                                        </div>
+
+                                        <!--content featured gallery here -->
+                                    </div>
+                                    <script type="text/javascript">
+                                        if (!window.slider) var slider = {};
+                                        slider.data = [
+                                            {"id": "slide-img-1", "client": "nature beauty", "desc": "nature beauty photography"},
+                                            {"id": "slide-img-2", "client": "nature beauty", "desc": "add your description here"},
+                                            {"id": "slide-img-3", "client": "nature beauty", "desc": "add your description here"},
+                                            {"id": "slide-img-4", "client": "nature beauty", "desc": "add your description here"},
+                                            {"id": "slide-img-5", "client": "nature beauty", "desc": "add your description here"},
+                                            {"id": "slide-img-6", "client": "nature beauty", "desc": "add your description here"},
+                                            {"id": "slide-img-7", "client": "nature beauty", "desc": "add your description here"}
+                                        ];
+                                    </script>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!--end slide image-->
+
 <table class="mainbody" border="0" cellspacing="0" cellpadding="0">
 <tr valign="top">
 <!--begin leftcolumn-->
@@ -494,16 +570,16 @@ $baseUrl = base_url();
                                 <a id="menu85" href="#">กล้องวงจรปิด</a>
                             </td>
                         </tr>
-<!--                        <tr>-->
-<!--                            <td>-->
-<!--                                <a id="menu122" href="#">References</a>-->
-<!--                            </td>-->
-<!--                        </tr>-->
-<!--                        <tr>-->
-<!--                            <td class="last">-->
-<!--                                <a id="menu144" href="#">Accomplishments</a>-->
-<!--                            </td>-->
-<!--                        </tr>-->
+                        <!--                        <tr>-->
+                        <!--                            <td>-->
+                        <!--                                <a id="menu122" href="#">References</a>-->
+                        <!--                            </td>-->
+                        <!--                        </tr>-->
+                        <!--                        <tr>-->
+                        <!--                            <td class="last">-->
+                        <!--                                <a id="menu144" href="#">Accomplishments</a>-->
+                        <!--                            </td>-->
+                        <!--                        </tr>-->
                     </table>
                 </div>
                 <div id="subwrap">
